2314a612547916fc9ebcd1d4a8ed4c42e503126e
Nathan Ellison <nathan.ipadman@gmail.com>
2021-04-06 14:21:27 +1200

    Revising game controller script

--- Assets/Scripts/GameControllerFinal.cs
+++ Assets/Scripts/GameControllerFinal.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class GameControllerFinal : MonoBehaviour
 {
     //Import assets that are going to be created whena burger is being made
-
+    public GameObject patty;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +18,27 @@ public class GameControllerFinal : MonoBehaviour
     void Update()
     {
         //Add functions for each of the controls
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            //Spawn in a new uncooked patty
+            Instantiate(patty, new Vector3(2, 0), Quaternion.identity);
+        }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            //Flip uncooked patty
+        }
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            //Move patty off the patty
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            //Place down bottom bun
+        }
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            //Place down cooked patty
+        }
+
     }
 }
