a7dea49c857965b4ff433590d4aa8d69be591647
Nathan Ellison <nathan.ipadman@gmail.com>
2021-06-27 16:18:47 +1200

    Final scores are now uploaded to an external server and will soon be able to be viewed on a webpage (further configuration is required)

--- Assets/ScreenManager.cs
+++ Assets/ScreenManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.IO;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.Networking;
 
 public class ScreenManager : MonoBehaviour
 {
@@ -21,12 +22,42 @@ public class ScreenManager : MonoBehaviour
 
         scoreText.text = "Final score: " + line;
 
+        //Upload score to server
+        StartCoroutine(UploadScore(line));
+
         File.Delete(Application.dataPath + "/score.txt");
+
     }
 
     public void BackToMain()
     {
         //Load the main menu
         SceneManager.LoadScene(0);
+    }
+
+    IEnumerator UploadScore(string line)
+    {
+        //Create new form to send data in
+        WWWForm form = new WWWForm();
+
+        //Add the data
+        form.AddField("name", "test");
+        form.AddField("score", int.Parse(line));
+
+        Debug.Log(int.Parse(line));
+
+        //Select destination and send data
+        UnityWebRequest www = UnityWebRequest.Post("https://nathan-ellison.com/pages/add.php", form);
+        yield return www.SendWebRequest();
+
+        //Check the result
+        if(www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete");
+        }
     }
 }
